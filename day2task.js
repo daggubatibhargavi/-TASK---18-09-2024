ce985aa88cd3dd1ecba17aa6a701c7d21d8b2c6c
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2024-09-22 11:53:26 +0530

    Add files via upload

---
+++ day2task.js
@@ -0,0 +1,28 @@
+//primitive datatypes
+var first_name = "bhargavi";
+last_name = "Daggubati";
+console.log(first_name, last_name);
+var num = 20;
+num = 10; // Changing the value of the "number" property
+console.log(num);
+var x = null; //null
+console.log(x);
+var y; //undefined
+console.log(y);
+let isAdult = false; // boolean datatype
+var age = 20;
+if (age >= 18) {
+  isAdult = true;
+}
+console.log(isAdult);
+//non-primitive datatypes
+var details = {
+  name: "bhargavi",
+  password: 1234,
+};
+console.log(details.name);
+console.log(details.password);
+var arr = [1, 2, 3, 4];
+console.log(arr[2]);
+arr[3] = 5;
+console.log(arr);
